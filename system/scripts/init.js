0b9d61a73b45e6443c4d5deb8a149318216f9002
AdamBravo79 <adambravo79.games@gmail.com>
2026-01-17 15:59:22 -0300

    feat: ultimos ajustes para subir versão

--- system/scripts/init.js
+++ system/scripts/init.js
@@ -1,27 +1,39 @@
+import { GambiarraActor } from "./actor.js";
+import { GambiarraActorSheet } from "./actor-sheet.js";
+import { GambiarraItem } from "./itens.js";
+import { GambiarraItemSheet } from "./item-sheet.js";
+
 Hooks.once("init", () => {
-  console.log("🪢 GAMBIARRA.SYS6 | Inicializando v0.4");
+  console.log("🪢 GAMBIARRA.SYS6 | Inicializando sistema");
 
   CONFIG.Actor.documentClass = GambiarraActor;
   CONFIG.Item.documentClass = GambiarraItem;
 
   Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet("gambiarra", GambiarraActorSheet, {
+  Actors.registerSheet("gambiarra-sys6", GambiarraActorSheet, {
     types: ["character"],
     makeDefault: true
   });
 
   Items.unregisterSheet("core", ItemSheet);
-  Items.registerSheet("gambiarra", GambiarraItemSheet, {
+  Items.registerSheet("gambiarra-sys6", GambiarraItemSheet, {
+    types: ["item"],
     makeDefault: true
   });
 
+  Hooks.on("preCreateActor", (actor, data) => {
+    if (!data.type) actor.updateSource({ type: "character" });
+  });
+
   game.gambiarra = {
-    difficulties: {
-      normal: { label: "Normal", sucessos: 1, alvo: 4 },
-      complexo: { label: "Complexo", sucessos: 2, alvo: 4 },
-      bug: { label: "BUG Leve", sucessos: 1, alvo: 5 },
-      epico: { label: "Épico", sucessos: 2, alvo: 5 },
-      impossivel: { label: "Impossível", sucessos: 3, alvo: 6 }
+    config: {
+      difficulties: {
+        normal: { label: "Normal", dice: 1, target: 4 },
+        complexo: { label: "Complexo", dice: 2, target: 4 },
+        bug: { label: "BUG Leve", dice: 1, target: 5 },
+        epico: { label: "Épico", dice: 2, target: 5 },
+        impossivel: { label: "Impossível", dice: 3, target: 6 }
+      }
     }
   };
 });
